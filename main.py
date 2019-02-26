8e2ba7f42fa927794d7a35ed27049c10329313e2
TakamiChie@Visnu <chie@onpu-tamago.net>
2019-02-26 18:19:59 +0900

    print処理を再度追加天気情報の表示をWeatherクラスに移動

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ class Weather:
     if data["temperature"]["min"] is not None:
       self.temperature_min = data["temperature"]["min"]["celsius"]
 
+  def print(self):
+    print(self.date)
+    print("  " + self.telop)
+    if self.temperature_min is not None:
+      print("  最低気温:{0}".format(self.temperature_min))
+    if self.temperature_max is not None:
+      print("  最高気温:{0}".format(self.temperature_max))
+
 # 初期処理
 id = 140010
 url = "http://weather.livedoor.com/forecast/webservice/json/v1?city={0}"
@@ -29,9 +37,4 @@ with urllib.request.urlopen(req) as res:
     weathers.append(Weather(forecast))
   # 表示
   for item in weathers:
-    print(item.date)
-    print("  " + item.telop)
-    if item.temperature_min is not None:
-      print("  最低気温:{0}".format(item.temperature_min)) 
-    if item.temperature_max is not None:
-      print("  最高気温:{0}".format(item.temperature_max))
+    item.print()
